afd99e77af4a05b9bde224e422efd40cf9fb0b46
invaa <alex@zamkovyi.name>
2014-08-14 16:28:12 +0300

    autoboxing.java

---
+++ src/org/diosoft/javajuinior/Autoboxing.java
@@ -0,0 +1,58 @@
+package org.diosoft.javajuinior;
+
+public class Autoboxing {
+
+	static Integer add(Integer i, Integer j) {
+		System.out.print("add(Integer, Integer): ");
+		return i + j;
+	}
+	
+	static Integer add(Integer i, int j) {
+		System.out.print("add(Integer, int): ");
+		return i + j;
+	}
+	
+	static Integer add(int i, Integer j) {
+		System.out.print("add(int, Integer): ");
+		return i + j;
+	}
+	
+	static Integer add(int i, int j) {
+		System.out.print("add(int, int): ");
+		return i + j;
+	}
+	
+	static Integer add(int ...i) {
+		System.out.print("add(int ...i): ");	
+		int res = 0;
+		
+		for (int index = 0; index < i.length; index++) {
+			res+=i[index];
+		}
+		return res;
+	}
+	
+	static Integer add(Integer ...i) {
+		System.out.print("add(int ...i): ");
+		int res = 0;
+		
+		for (int index = 0; index < i.length; index++) {
+			res+=i[index];
+		}
+		return res;
+	}
+	
+	public static void main(String[] args) {
+	
+		System.out.print("adding: ");
+		System.out.println(add(1,2));
+		
+		int a = 3, b = 4;
+		
+		System.out.print("adding: ");
+		System.out.println(add(a,b));
+		
+
+	}
+
+}
